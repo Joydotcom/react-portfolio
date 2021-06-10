3450b4dfcbc8349fc5d2e89f2d988f61513e0f8c
joydotcom <msjoyjackson@gmail.com>
2021-06-10 15:24:54 -0400

    added pdf, changed padding in navbar

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { Document, Page } from 'react-pdf';
-
+import "./style.css";
 import pdf from "./img/Resume21.pdf";
+import { Component } from 'react';
+import { pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
-// function Resume() {
-//   return (
-//     <Document file={pdf}>
-//     <Page pageNumber={1} />
-//   </Document>
-//   );
-// }
 
-// export default Resume; 
-
-export default function Resume() {
+function Resume() {
   return (
-    <Document file={pdf}>
-      <Page pageNumber={0} />
-    </Document>
+    <div className="pdf">
+  <Document file={pdf}>
+<Page pageNumber={1} />
+  </Document>
+  </div>
   );
 }
+
+export default Resume; 
+
+{/* // export default function Resume() { */}
+{/* //   return (
+//     <Document file={pdf}>
+//       <Page pageNumber={0} />
+//     </Document>
+//   );
+// }
 
 // import React, { useState } from 'react';
 // import { Document, Page } from 'react-pdf';
@@ -29,24 +35,24 @@ export default function Resume() {
 
 
 
-// function Resume() {
-//   const [numPages, setNumPages] = useState(null);
+// function Resume() { */}
+{/* //   const [numPages, setNumPages] = useState(null);
 //   const [pageNumber, setPageNumber] = useState(1);
 
-//   function onDocumentLoadSuccess({ numPages }) {
-//     setNumPages(numPages);
+//   function onDocumentLoadSuccess({ numPages }) { */}
+{/* //     setNumPages(numPages);
 //   }
 
 //   return (
 //     <div>
-//       <Document
-//         file="./img/Resume21.pdf"
+//       <Document */}
+{/* //         file="./img/Resume21.pdf"
 //         onLoadSuccess={onDocumentLoadSuccess}
 //       >
 //         <Page pageNumber={1} />
-//       </Document>
-//       <p>Page {1} of {1}</p>
+//       </Document> */}
+{/* //       <p>Page {1} of {1}</p>
 //     </div>
 //   );
 // }
-// export default  Resume;
+// export default  Resume; */}
