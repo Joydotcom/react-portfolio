32982d660951d9ded15dade98969fb51a1177aac
joydotcom <msjoyjackson@gmail.com>
2021-06-15 17:18:32 -0400

    revised resume

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -19,35 +19,39 @@ function Home() {
         <img className="image" src={joy} width="600px" alt="headshot" />
       </div>
       {/* <h1></h1> */}
-      <p className="bio" style={{
+      <p
+        className="bio"
+        style={{
           // display: "flex",
           justifyContent: "center",
           alignItems: "center",
-        }}>
-        As a recent graduate of Georgia Tech's, Full
-        Stack Web Development bootcamp, I am eager to continue my professional
-        experience in the Tech Industry. I have a B.S. in Kinesiology, a
-        professional teaching certification in Performance Art and Design, and a
-        certification in Full Stack Web Development.
-        <br></br>
-        I have been a professional educator for 12+ years, having
+        }}
+      >
+        As a recent graduate of Georgia Tech's, Full Stack Web Development
+        bootcamp, I am eager to continue my professional experience in the Tech
+        Industry. I have a B.S. in Kinesiology, a professional teaching
+        certification in Performance Art and Design, and a certification in Full
+        Stack Web Development.
+        <br></br>I have been a professional educator for 12+ years, having
         experience as a Student Development Coordinator and Performance Art and
         Design Teacher. Through my Educational Leadership graduate studies, I
-        was successful in the implementation and facilitation of student success programming and professional
-        staff development.
-      <br></br>
-        In the field of education, we are required to wear many hats. Educators
-        must be flexible, juggle multiple concerns at once, and effectively
-        communicate with students, team members, and parents (sometimes irate).
-        Attention to detail is an understatement in a setting where children
-        are top priority. We must be able to quickly assess any situation and
-        rely on critical thinking to utilize the proper strategies and skills
-        for problem solving – professionally and accurately. These skills I
-        have, and continue to, successfully develop.
-       I am certain that these skills translate well into the Tech
-        Industry, and will be an asset to any position, team, and company.
-     <br></br>
-  <br></br>
+        was successful in the implementation and facilitation of student success
+        programming and professional staff development.
+        <br></br>I also had the pleasure of working in the services industry
+        part time for many years as an Assistant Creator in Nightlife for the
+        Atlantic City Casinos and Resorts. My role included assisting the
+        Director with Nightlife Entertainment Procedures and Protocol,
+        overseeing Services for Entertainment Guests, presenting data, and
+        conducting rehearsals. In all of my roles I must be flexible, juggle
+        multiple concerns at once, and effectively communicate with my team.
+        Attention to detail is always a top priority. I must be able to quickly
+        assess any situation and rely on critical thinking to utilize the proper
+        strategies and skills for problem solving – professionally and
+        accurately.<br></br> I am certain that these skills translate well into
+        the Tech Industry, and will be an asset to any position, team, and
+        company.
+        <br></br>
+        <br></br>
         <span className="joy">
           Joy <FontAwesomeIcon icon={faHandPeace} />{" "}
         </span>
